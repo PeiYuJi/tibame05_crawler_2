5c079c7e3f9b5ea2f876e4487e79697c2df358ff
pei yu ji <abc0970870@gmail.com>
2025-10-06 23:38:18 +0800

    commit

--- crawler/tasks_crawler_etf_us.py
+++ crawler/tasks_crawler_etf_us.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import yfinance as yf
-import pandas_ta as ta
 import numpy as np
 from crawler.worker import app
 from database.main import write_etf_daily_price_to_db
@@ -38,33 +37,10 @@ def crawler_etf_us(etf_list_df):
             continue
         df.columns = df.columns.droplevel(1)  # 把 'Price' 這層拿掉
 
-        # RSI (14) (相對強弱指標)
-        df["rsi"] = ta.rsi(df["close"], length=14)
-
-        # MA5、MA20（移動平均線）（也可以使用 df['close'].rolling(5).mean())）
-        df["ma5"] = ta.sma(df["close"], length=5)
-        df["ma20"] = ta.sma(df["close"], length=20)
-
-        # MACD（移動平均收斂背離指標）
-        macd = ta.macd(df["close"], fast=12, slow=26, signal=9)
-        df["macd_line"] = macd["MACD_12_26_9"]
-        df["macd_signal"] = macd["MACDs_12_26_9"]
-        df["macd_hist"] = macd["MACDh_12_26_9"]
-
-        # KD 指標（STOCH: 隨機震盪指標）
-        stoch = ta.stoch(df["high"], df["low"], df["close"], k=14, d=3, smooth_k=3)
-        df["pct_k"] = stoch["STOCHk_14_3_3"]
-        df["pct_d"] = stoch["STOCHd_14_3_3"]
-
-        # 增加該日報酬率與累積報酬指數
-        df['daily_return'] = df['adj_close'].pct_change()
-        df['cumulative_return'] = (1 + df['daily_return']).cumprod()
         df.insert(0, "etf_id", r)  # 新增一欄「etf_id」
         print (df)
         #df.columns = ["etf_id","date", "dividend_per_unit"]    # 調整欄位名稱
-        columns_order = ['etf_id', 'date', 'adj_close','close','high', 'low', 'open','volume',
-                        'rsi', 'ma5', 'ma20', 'macd_line', 'macd_signal', 'macd_hist',
-                        'pct_k', 'pct_d', 'daily_return', 'cumulative_return']
+        columns_order = ['etf_id', 'date', 'adj_close','close','high', 'low', 'open','volume']
         df = df[columns_order]
 
         
